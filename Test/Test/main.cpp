c1b2ebe6d902671b449871b66d2cc88dadc025a8
kathryn-choi <mody123@naver.com>
2017-11-16 19:54:05 +0900

    Mod main.cpp
    from 5x5  to 6x4

--- Test/Test/main.cpp
+++ Test/Test/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
 	
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 6; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < 4; j++)
 		{
-			cout << "*";
+			cout << "$";
 		}
 		cout << endl;
 	}
